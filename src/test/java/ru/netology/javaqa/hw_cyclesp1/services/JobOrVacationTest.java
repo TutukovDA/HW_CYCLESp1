304809464c4dc05f0c3acb78535b308f46f613c9
Dmitriy Tyutyukov <dmitriy.tyutyukov@ifellow.ru>
2023-12-14 17:00:44 +0300

    added task2

--- src/test/java/ru/netology/javaqa/hw_cyclesp1/services/JobOrVacationTest.java
+++ src/test/java/ru/netology/javaqa/hw_cyclesp1/services/JobOrVacationTest.java
@@ -1,22 +1,15 @@
 package ru.netology.javaqa.hw_cyclesp1.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class JobOrVacationTest {
-    @Test
-    public void JobORVacationTest() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resourses/data")
+    public void JobORVacationTest(int expected, int deposit, int income, int expenses, int threshold) {
         JobOrVacation count = new JobOrVacation();
-        int expected = 2;
-        int actual = count.countVacationMonth(0, 100000, 60000, 150000);
-        Assertions.assertEquals(expected,actual);
-        System.out.println("Фрилансер может отдыхать " + actual + " месяцев/ца");
-    }
-    @Test
-    public void JobORVacationTest2() {
-        JobOrVacation count = new JobOrVacation();
-        int expected = 3;
-        int actual = count.countVacationMonth(0, 10000, 3000, 20000);
+        int actual = count.countVacationMonth(deposit,income,expenses,threshold);
         Assertions.assertEquals(expected,actual);
         System.out.println("Фрилансер может отдыхать " + actual + " месяцев/ца");
     }
